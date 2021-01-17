8ff2af982622df17141e71894c854ebe84d3bdd3
Anton Talanov <dotollia@mail.ru>
2021-01-18 02:25:44 +0400

    create service

---
+++ src/main/java/ru/neoflex/msa/training/kafkaproducer/KafkaProducerController.java
@@ -0,0 +1,29 @@
+package ru.neoflex.msa.training.kafkaproducer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@RestController
+public class KafkaProducerController {
+
+    private static final String HOST = "10.20.30.10";
+    private static final String PORT = "8080";
+    private static final String PATH = "";
+
+    @Autowired
+    private KafkaTemplate<String, BankAccount> kafkaTemplate;
+
+    @GetMapping
+    public void sendToTopic() {
+        RestTemplate restTemplate = new RestTemplate();
+        BankAccount bankAccount = restTemplate.getForObject("http://" + HOST + ":" + PORT + PATH  + "/", BankAccount.class);
+        System.out.println(bankAccount);
+        String msgId = UUID.randomUUID().toString();
+        kafkaTemplate.send("bankAccountTopic", msgId, bankAccount);
+    }
+}
