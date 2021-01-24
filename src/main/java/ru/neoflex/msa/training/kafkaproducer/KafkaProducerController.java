884200e6fe06a333d374b9809087d2703b4bf432
Anton Talanov <dotollia@mail.ru>
2021-01-25 01:50:04 +0400

    add properties for BankAccounGenerator url

--- src/main/java/ru/neoflex/msa/training/kafkaproducer/KafkaProducerController.java
+++ src/main/java/ru/neoflex/msa/training/kafkaproducer/KafkaProducerController.java
@@ -1,6 +1,7 @@
 package ru.neoflex.msa.training.kafkaproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,8 @@ import java.util.UUID;
 @RestController
 public class KafkaProducerController {
 
-    private static final String HOST = "10.20.30.15";
-    private static final String PORT = "8080";
-    private static final String PATH = "";
+    @Value("${application.bankAccountGenerator.url}")
+    private String BANK_ACCOUNT_GENERATOR_URL;
 
     @Autowired
     private KafkaTemplate<String, BankAccount> kafkaTemplate;
@@ -22,7 +22,7 @@ public class KafkaProducerController {
     @GetMapping
     public void sendToTopic() {
         RestTemplate restTemplate = new RestTemplate();
-        BankAccount bankAccount = restTemplate.getForObject("http://" + HOST + ":" + PORT + PATH  + "/", BankAccount.class);
+        BankAccount bankAccount = restTemplate.getForObject("http://" + BANK_ACCOUNT_GENERATOR_URL, BankAccount.class);
         System.out.println(bankAccount);
         String msgId = UUID.randomUUID().toString();
         kafkaTemplate.send("bankAccountTopic", msgId, bankAccount);
